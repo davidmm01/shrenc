79da1b0e737cd29a4e043d697462870250e71546
David Michieli <d.m.michieli@gmail.com>
2022-06-19 14:26:29 +1000

    split enc and dec stack into classes

--- app/gui.py
+++ app/gui.py
@@ -27,15 +27,10 @@ SELECTED_FILE_DEC_RESET_MSG = "File to decrypt: None chosen"
 
 
 class MainWindow(Gtk.Window):
-    # TODO: oh god this init is very gross, address before its too late...
-    # maybe start with an encryption stack class?
     def __init__(self):
         Gtk.Window.__init__(self, title="Shrenc")
         self.set_border_width(10)
         Gtk.Window.set_default_size(self, 640, 480)
-
-        self._selected_enc_filename = None
-        self._selected_dec_filename = None
 
         # Create the outer vertical box with a space of 100 pixel.
         outer_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
@@ -46,6 +41,30 @@ class MainWindow(Gtk.Window):
         stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
         stack.set_transition_duration(1000)
 
+        # add encrypt box to encrypt stack
+        encryption_stack = EncryptionStack(self)
+        stack.add_titled(encryption_stack.encrypt_box, "encrypt_box", "Encrypt")
+
+        # add decrypt box to encrypt stack
+        decrypt_stack = DecryptionStack(self)
+        stack.add_titled(decrypt_stack.decrypt_box, "decrypt_box", "Decrypt")
+
+        # Implementation of stack switcher.
+        stack_switcher = Gtk.StackSwitcher()
+        stack_switcher.set_stack(stack)
+
+        # Put the stack switcher and the stack into the outer box
+        outer_box.pack_start(stack_switcher, True, True, 0)
+        outer_box.pack_start(stack, True, True, 0)
+
+        # Add the outer box to the window
+        self.add(outer_box)
+
+
+class EncryptionStack:
+    def __init__(self, main_window):
+        self.main_window = main_window
+        self._selected_enc_filename = None
         # make choose file and encrypt buttons
         choose_file_enc_button = Gtk.Button(label="Choose File")
         choose_file_enc_button.connect("clicked", self.on_choose_file_enc_clicked)
@@ -119,84 +138,22 @@ class MainWindow(Gtk.Window):
         self.encrypt_button.set_sensitive(False)
 
         # create the box that will home all the encryption elements, and put the buttons in
-        encrypt_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-        encrypt_box.pack_start(choose_file_enc_button, True, True, 0)
+        self.encrypt_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        self.encrypt_box.pack_start(choose_file_enc_button, True, True, 0)
         # TODO: this choose file needs to be a "choose file OR folder"
-        encrypt_box.pack_start(self.chosen_file_enc_label, True, True, 0)
-        encrypt_box.pack_start(self.entry_passphrase_label, True, True, 0)
-        encrypt_box.pack_start(self.entry_passphrase, True, True, 0)
-        encrypt_box.pack_start(self.entry_passphrase_toggle, True, True, 0)
-        encrypt_box.pack_start(self._selected_compression_label, True, True, 0)
-        encrypt_box.pack_start(self.compression_combo, True, True, 0)
-        encrypt_box.pack_start(self.armor_toggle, True, True, 0)
-        encrypt_box.pack_start(self._select_cypher_label, True, True, 0)
-        encrypt_box.pack_start(self.cypher_combo, True, True, 0)
-        encrypt_box.pack_start(
+        self.encrypt_box.pack_start(self.chosen_file_enc_label, True, True, 0)
+        self.encrypt_box.pack_start(self.entry_passphrase_label, True, True, 0)
+        self.encrypt_box.pack_start(self.entry_passphrase, True, True, 0)
+        self.encrypt_box.pack_start(self.entry_passphrase_toggle, True, True, 0)
+        self.encrypt_box.pack_start(self._selected_compression_label, True, True, 0)
+        self.encrypt_box.pack_start(self.compression_combo, True, True, 0)
+        self.encrypt_box.pack_start(self.armor_toggle, True, True, 0)
+        self.encrypt_box.pack_start(self._select_cypher_label, True, True, 0)
+        self.encrypt_box.pack_start(self.cypher_combo, True, True, 0)
+        self.encrypt_box.pack_start(
             self.encrypt_button, True, True, 0
         )  # TODO: figure out these other params
-        encrypt_box.pack_start(self.enc_outcome_label, True, True, 0)
-
-        # add encrypt box to encrypt stack
-        stack.add_titled(encrypt_box, "encrypt_box", "Encrypt")
-
-        # Decrypt
-        choose_file_dec_button = Gtk.Button(label="Choose File")
-        choose_file_dec_button.connect("clicked", self.on_choose_file_dec_clicked)
-        self.chosen_file_dec_label = Gtk.Label(label=SELECTED_FILE_DEC_RESET_MSG)
-
-        # TODO: should not be able to hit encrypt without a passphrase
-        self._reveal_passphrase_dec = False
-        self.entry_passphrase_label = Gtk.Label(label="Enter encryption passphrase")
-        self.entry_passphrase_dec = Gtk.Entry()
-        self.entry_passphrase_dec.set_visibility(self._reveal_passphrase_enc)
-        self.entry_passphrase_dec.connect(
-            "changed", self.on_dec_entry_passphrase_changed
-        )
-        self._dec_passphrase_entered = None
-        self.entry_passphrase_toggle_dec = Gtk.CheckButton(label="Reveal passphrase?")
-        self.entry_passphrase_toggle_dec.connect(
-            "toggled", self.on_entry_passphrase_dec_toggled
-        )
-        self._decrypt_location = "./OUTPUT"
-        self.choose_dec_location_button = Gtk.Button(label="Select decryption location")
-        self.choose_dec_location_button.connect(
-            "clicked", self.on_choose_dec_location_button_clicked
-        )
-        self.decrypt_location_label = Gtk.Label(
-            label=f"Selected output location: {self._decrypt_location}"
-        )
-
-        self.dec_outcome_label = Gtk.Label(label="")
-        self.decrypt_button = Gtk.Button(label="Decrypt")
-        self.decrypt_button.connect("clicked", self.on_decrypt_clicked)
-        self.decrypt_button.set_sensitive(False)
-
-        # create the box that will home all the decryption elements, and put the buttons in
-        decrypt_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-        decrypt_box.pack_start(choose_file_dec_button, True, True, 0)
-        decrypt_box.pack_start(self.chosen_file_dec_label, True, True, 0)
-        decrypt_box.pack_start(self.entry_passphrase_dec, True, True, 0)
-        decrypt_box.pack_start(self.entry_passphrase_toggle_dec, True, True, 0)
-        decrypt_box.pack_start(self.choose_dec_location_button, True, True, 0)
-        decrypt_box.pack_start(self.decrypt_location_label, True, True, 0)
-        decrypt_box.pack_start(
-            self.decrypt_button, True, True, 0
-        )  # TODO: figure out these other params
-        decrypt_box.pack_start(self.dec_outcome_label, True, True, 0)
-
-        # add decrypt box to encrypt stack
-        stack.add_titled(decrypt_box, "decrypt_box", "Decrypt")
-
-        # Implementation of stack switcher.
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-
-        # Put the stack switcher and the stack into the outer box
-        outer_box.pack_start(stack_switcher, True, True, 0)
-        outer_box.pack_start(stack, True, True, 0)
-
-        # Add the outer box to the window
-        self.add(outer_box)
+        self.encrypt_box.pack_start(self.enc_outcome_label, True, True, 0)
 
     def on_cypher_combo_changed(self, combo):
         tree_iter = combo.get_active_iter()
@@ -212,10 +169,11 @@ class MainWindow(Gtk.Window):
             self._selected_compression = model[tree_iter][1]
             print("selected compression:", self._selected_compression)
 
-    # TODO: refactor following two methods DRY
     def on_choose_file_enc_clicked(self, widget):
         dialog = Gtk.FileChooserDialog(
-            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
+            title="Please choose a file",
+            parent=self.main_window,
+            action=Gtk.FileChooserAction.OPEN,
         )
         dialog.add_buttons(
             Gtk.STOCK_CANCEL,
@@ -233,55 +191,6 @@ class MainWindow(Gtk.Window):
                 "File to encrypt: " + self._selected_enc_filename
             )
             self.update_encryption_button_sensitivity()
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def on_choose_file_dec_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-            self._selected_dec_filename = dialog.get_filename()
-            self.chosen_file_dec_label.set_text(
-                "File to decrypt: " + self._selected_dec_filename
-            )
-            self.update_decryption_button_sensitivity()
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def on_choose_dec_location_button_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a folder",
-            parent=self,
-            action=Gtk.FileChooserAction.SELECT_FOLDER,
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(800, 400)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
-            self._decrypt_location = dialog.get_filename()
-            self.decrypt_location_label.set_text(
-                f"Selected output location: {self._decrypt_location}"
-            )
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
 
@@ -319,6 +228,122 @@ class MainWindow(Gtk.Window):
         self._selected_enc_filename = None
         self.encrypt_button.set_sensitive(False)
 
+    def on_enc_entry_passphrase_changed(self, widget):
+        self._enc_passphrase_entered = self.entry_passphrase.get_text()
+        self.update_encryption_button_sensitivity()
+
+    def on_entry_passphrase_toggled(self, checkbutton):
+        # is it necessary to have the self._reveal_passphrase_enc attr?
+        self._reveal_passphrase_enc = self.entry_passphrase_toggle.get_active()
+        self.entry_passphrase.set_visibility(self._reveal_passphrase_enc)
+
+    def update_encryption_button_sensitivity(self):
+        print("self._enc_passphrase_entered:", self._enc_passphrase_entered)
+        print("self._selected_enc_filename:", self._selected_enc_filename)
+
+        sensitivity = self._enc_passphrase_entered and self._selected_enc_filename
+        self.encrypt_button.set_sensitive(sensitivity)
+
+
+class DecryptionStack:
+    def __init__(self, main_window):
+        self.main_window = main_window
+        self._selected_dec_filename = None
+        choose_file_dec_button = Gtk.Button(label="Choose File")
+        choose_file_dec_button.connect("clicked", self.on_choose_file_dec_clicked)
+        self.chosen_file_dec_label = Gtk.Label(label=SELECTED_FILE_DEC_RESET_MSG)
+
+        # TODO: should not be able to hit encrypt without a passphrase
+        self._reveal_passphrase_dec = False
+        self.entry_passphrase_label = Gtk.Label(label="Enter encryption passphrase")
+        self.entry_passphrase_dec = Gtk.Entry()
+        self.entry_passphrase_dec.set_visibility(self._reveal_passphrase_dec)
+        self.entry_passphrase_dec.connect(
+            "changed", self.on_dec_entry_passphrase_changed
+        )
+        self._dec_passphrase_entered = None
+        self.entry_passphrase_toggle_dec = Gtk.CheckButton(label="Reveal passphrase?")
+        self.entry_passphrase_toggle_dec.connect(
+            "toggled", self.on_entry_passphrase_dec_toggled
+        )
+        self._decrypt_location = "./OUTPUT"
+        self.choose_dec_location_button = Gtk.Button(label="Select decryption location")
+        self.choose_dec_location_button.connect(
+            "clicked", self.on_choose_dec_location_button_clicked
+        )
+        self.decrypt_location_label = Gtk.Label(
+            label=f"Selected output location: {self._decrypt_location}"
+        )
+
+        self.dec_outcome_label = Gtk.Label(label="")
+        self.decrypt_button = Gtk.Button(label="Decrypt")
+        self.decrypt_button.connect("clicked", self.on_decrypt_clicked)
+        self.decrypt_button.set_sensitive(False)
+
+        # create the box that will home all the decryption elements, and put the buttons in
+        self.decrypt_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        self.decrypt_box.pack_start(choose_file_dec_button, True, True, 0)
+        self.decrypt_box.pack_start(self.chosen_file_dec_label, True, True, 0)
+        self.decrypt_box.pack_start(self.entry_passphrase_dec, True, True, 0)
+        self.decrypt_box.pack_start(self.entry_passphrase_toggle_dec, True, True, 0)
+        self.decrypt_box.pack_start(self.choose_dec_location_button, True, True, 0)
+        self.decrypt_box.pack_start(self.decrypt_location_label, True, True, 0)
+        self.decrypt_box.pack_start(
+            self.decrypt_button, True, True, 0
+        )  # TODO: figure out these other params
+        self.decrypt_box.pack_start(self.dec_outcome_label, True, True, 0)
+
+    def on_choose_file_dec_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a file",
+            parent=self.main_window,
+            action=Gtk.FileChooserAction.OPEN,
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK,
+        )
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self._selected_dec_filename = dialog.get_filename()
+            self.chosen_file_dec_label.set_text(
+                "File to decrypt: " + self._selected_dec_filename
+            )
+            self.update_decryption_button_sensitivity()
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def on_choose_dec_location_button_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a folder",
+            parent=self.main_window,
+            action=Gtk.FileChooserAction.SELECT_FOLDER,
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+        )
+        dialog.set_default_size(800, 400)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Select clicked")
+            print("Folder selected: " + dialog.get_filename())
+            self._decrypt_location = dialog.get_filename()
+            self.decrypt_location_label.set_text(
+                f"Selected output location: {self._decrypt_location}"
+            )
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
     def on_decrypt_clicked(self, widget):
         print("decrypt was clicked!!!!!!!")
         decryted_name = self._selected_dec_filename.rstrip(".enc")
@@ -340,29 +365,13 @@ class MainWindow(Gtk.Window):
         self._selected_dec_filename = None
         self.update_decryption_button_sensitivity()
 
-    def on_enc_entry_passphrase_changed(self, widget):
-        self._enc_passphrase_entered = self.entry_passphrase.get_text()
-        self.update_encryption_button_sensitivity()
-
     def on_dec_entry_passphrase_changed(self, widget):
         self._dec_passphrase_entered = self.entry_passphrase_dec.get_text()
         self.update_decryption_button_sensitivity()
 
-    def on_entry_passphrase_toggled(self, checkbutton):
-        # is it necessary to have the self._reveal_passphrase_enc attr?
-        self._reveal_passphrase_enc = self.entry_passphrase_toggle.get_active()
-        self.entry_passphrase.set_visibility(self._reveal_passphrase_enc)
-
     def on_entry_passphrase_dec_toggled(self, checkbutton):
         self._reveal_passphrase_dec = self.entry_passphrase_toggle_dec.get_active()
         self.entry_passphrase_dec.set_visibility(self._reveal_passphrase_dec)
-
-    def update_encryption_button_sensitivity(self):
-        print("self._enc_passphrase_entered:", self._enc_passphrase_entered)
-        print("self._selected_enc_filename:", self._selected_enc_filename)
-
-        sensitivity = self._enc_passphrase_entered and self._selected_enc_filename
-        self.encrypt_button.set_sensitive(sensitivity)
 
     def update_decryption_button_sensitivity(self):
         sensitivity = self._dec_passphrase_entered and self._selected_dec_filename
